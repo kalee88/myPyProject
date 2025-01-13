a4df4a5dd1663308965e657d887ee4c365c1f783
Kyle Lee <117378813+kalee88@users.noreply.github.com>
2025-01-12 19:38:55 -0500

    Update PokemonShowdownBattle.py

--- PokemonShowdownBattle.py
+++ PokemonShowdownBattle.py
@@ -1,12 +1,12 @@
 class Pokemon:
-    def __init__(self, name, health , typing, attack, currentHealth):
+    def __init__(self, name, health, typing, attack, currentHealth):
         self.name = name
         self.health = health
         self.typing = typing
         self.attack = attack
         self.currentHealth = currentHealth
     
-    def setHealth(self,health):
+    def setHealth(self, health):
         self.health = health
     
     def getHealth(self):
@@ -14,11 +14,24 @@ class Pokemon:
     
     def getName(self):
         return self.name
-def pokemonInformation(Pokemon):
-    return Pokemon.name + " has " + str(Pokemon.health) + " health "
+    
+    def perform_attack(self, other_pokemon):
+        # Ensure `other_pokemon` is an instance of the Pokemon class
+        if isinstance(other_pokemon, Pokemon):
+            other_pokemon.health -= 2
+            self.health += 1
 
+def pokemonInformation(pokemon):
+    return f"{pokemon.name} has {pokemon.health} health"
 
+# Create two Pokemon instances
 Garchomp = Pokemon("Garchomp", 5, "Dragon", 3, 5)
 Dragonite = Pokemon("Dragonite", 8, "Dragon", 3, 8)
-print(pokemonInformation(Garchomp))
-print(pokemonInformation(Dragonite))
+
+# Dragonite attacks Garchomp
+Dragonite.perform_attack(Garchomp)
+
+# Print information about both Pokemon
+print(pokemonInformation(Garchomp))  # Garchomp's health
+print(pokemonInformation(Dragonite))  # Dragonite's health
+
