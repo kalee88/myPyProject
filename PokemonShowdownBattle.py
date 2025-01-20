90b1009fa15cd4f1ac4a8e96f1ecaf9aae6afc8c
Kyle Lee <117378813+kalee88@users.noreply.github.com>
2025-01-19 19:46:46 -0500

    Update PokemonShowdownBattle.py

--- PokemonShowdownBattle.py
+++ PokemonShowdownBattle.py
@@ -1,10 +1,11 @@
 class Pokemon:
-    def __init__(self, name, health, typing, attack, currentHealth):
+    def __init__(self, name, health, typing, attack, currentHealth,speed):
         self.name = name
         self.health = health
         self.typing = typing
         self.attack = attack
         self.currentHealth = currentHealth
+        self.speed = speed
     
     def setHealth(self, health):
         self.health = health
@@ -34,10 +35,58 @@ class Pokemon:
     def perform_attackboost(self):
         self.attack = self.attack + 2
         
-        
+def askMove():
+    user_input = input("Press 1 to do attack number 1, Press 2 to do attack number 2, Press 3 to potion, Press 4 to give attack boost")
+    return user_input
+    
 def pokemonInformation(pokemon):
     return f"{pokemon.name} has {pokemon.health} health and {pokemon.attack} attack power"
 
+while True:
+    Latios = Pokemon("Latios", 8, "Dragon", 5, 8, 4)
+    Latias = Pokemon("Latias", 7, "Dragon", 6, 7, 5)
+    if (Latios.speed > Latias.speed):
+        x = askMove()
+        if (x == 1):
+            Latios.perform_attack(Latias)
+        if (x == 2):
+            Latios.perform_attack2(Latias)
+        if (x == 3):
+            Latios.perform_potion()
+        if (x == 4):
+            Latios.perform_attackboost()
+        y = askMove()
+        if (y == 1):
+            Latias.perform_attack(Latios)
+        if (y == 2):
+            Latias.perform_attack2(Latios)
+        if (y == 3):
+            Latias.perform_potion()
+        if (y == 4):
+            Latias.perform_attackboost()
+    else:
+        y = askMove()
+        if (y == 1):
+            Latias.perform_attack(Latios)
+        if (y == 2):
+            Latias.perform_attack2(Latios)
+        if (y == 3):
+            Latias.perform_potion()
+        if (y == 4):
+            Latias.perform_attackboost()
+        x = askMove()
+        if (x == 1):
+            Latios.perform_attack(Latias)
+        if (x == 2):
+            Latios.perform_attack2(Latias)
+        if (x == 3):
+            Latios.perform_potion()
+        if (x == 4):
+            Latios.perform_attackboost()
+    print(pokemonInformation(Latios))
+    print(pokemonInformation(Latias))
+    
+'''
 # Create two Pokemon instances
 Garchomp = Pokemon("Garchomp", 5, "Dragon", 3, 5)
 Dragonite = Pokemon("Dragonite", 8, "Dragon", 3, 8)
@@ -52,3 +101,4 @@ Garchomp.perform_potion()
 print(pokemonInformation(Garchomp))  # Garchomp's health
 Dragonite.perform_attackboost()
 print(pokemonInformation(Dragonite))  # Garchomp's health
+'''
